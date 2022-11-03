d032f16e0d0008e400df24fd6b75b5a1c0098e74
sergiozamoranomanzano <101356986+sergiozamoranomanzano@users.noreply.github.com>
2022-11-03 22:28:05 +0100

    Update EjemploGit.java
    He añadido la linea 9

--- EjemploGitDanielSergio/src/git/EjemploGit.java
+++ EjemploGitDanielSergio/src/git/EjemploGit.java
@@ -4,7 +4,9 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prueba de gir - Daniel");
+		System.out.println("Prueba de git - Daniel");
+		
+		System.out.println("Modificacion - Sergio");
 	}
 
 }
